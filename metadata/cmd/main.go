e0891251abf99fd533cf5131cef3dc20657af9fc
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-06-14 08:01:23 +0530

    feat: added metadata services for gRPC

--- metadata/cmd/main.go
+++ metadata/cmd/main.go
@@ -1,57 +1,71 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
 	"log"
-	"net/http"
-	"time"
+	"net"
 
+	"google.golang.org/grpc"
+	"movieexample.com/gen"
 	"movieexample.com/metadata/internal/controller/metadata"
-	httphandler "movieexample.com/metadata/internal/handler/http"
+	grpcHandler "movieexample.com/metadata/internal/handler/grpc"
 	"movieexample.com/metadata/internal/repository/memory"
-	"movieexample.com/pkg/discovery"
-	"movieexample.com/pkg/discovery/consul"
 )
 
-const serviceName = "metadata"
+// Below is the Implementation of HTTP with JSON communication
 
-func main() {
-	var port int
-	flag.IntVar(&port, "port", 8081, "API handler port")
-	flag.Parse()
-	log.Printf("Starting the metadata service on port %d", port)
+// const serviceName = "metadata"
 
-	registry, err := consul.NewRegistry("localhost:8500")
-	if err != nil {
-		panic(err)
-	}
+// func main() {
+// 	var port int
+// 	flag.IntVar(&port, "port", 8081, "API handler port")
+// 	flag.Parse()
+// 	log.Printf("Starting the metadata service on port %d", port)
 
-	ctx := context.Background()
-	instanceID := discovery.GenerateInstanceID(serviceName)
+// 	registry, err := consul.NewRegistry("localhost:8500")
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
-		panic(err)
-	}
+// 	ctx := context.Background()
+// 	instanceID := discovery.GenerateInstanceID(serviceName)
 
-	go func() {
-		for {
-			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
-				log.Println("Failed to report healthy state: " + err.Error())
-			}
-			time.Sleep(1 * time.Second)
-		}
-	}()
+// 	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+// 		panic(err)
+// 	}
 
-	defer registry.Deregister(ctx, instanceID, serviceName)
+// 	go func() {
+// 		for {
+// 			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+// 				log.Println("Failed to report healthy state: " + err.Error())
+// 			}
+// 			time.Sleep(1 * time.Second)
+// 		}
+// 	}()
 
+// 	defer registry.Deregister(ctx, instanceID, serviceName)
+
+// 	repo := memory.New()
+// 	ctrl := metadata.New(repo)
+// 	h := httphandler.New(ctrl)
+
+// 	http.Handle("/metdata", http.HandlerFunc(h.GetMetadata))
+// 	if err := http.ListenAndServe(":8081", nil); err != nil {
+// 		panic(err)
+// 	}
+// }
+
+// Below is the implementation of gRPC with Protobuf communicaiton b/w services
+func main(){
+	log.Println("Starting the movie metadata service")
 	repo := memory.New()
 	ctrl := metadata.New(repo)
-	h := httphandler.New(ctrl)
+	h := grpcHandler.New(ctrl)
+	lis, err := net.Listen("tcp", "localhost:8081")
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+	srv := grpc.NewServer()
+	gen.RegisterMetadataServiceServer(srv,h)
+	srv.Serve(lis)
 
-	http.Handle("/metdata", http.HandlerFunc(h.GetMetadata))
-	if err := http.ListenAndServe(":8081", nil); err != nil {
-		panic(err)
-	}
 }
---
+++ metadata/pkg/model/mapper.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"movieexample.com/gen"
+	model "movieexample.com/metadata/pkg"
+)
+
+// MetadataToProto converts a Metadata struct into a generated proto counterpart.
+func MetadataToProto(m *model.Metadata) *gen.Metadata {
+	return &gen.Metadata{
+		Id:          m.ID,
+        Title:       m.Title,
+        Description: m.Description,
+        Director:    m.Director,
+	}
+}
+
+// MetadataFromProto converts a generated proto counterpart 
+// into a Metadata struct.
+func MetadataFromProto(m *gen.Metadata) *model.Metadata {
+	return &model.Metadata{
+		ID:          m.Id,
+        Title:       m.Title,
+        Description: m.Description,
+        Director:    m.Director,
+	}
+}
